b3fab0ec0ed0beeea0c6d01900b043c16b774287
phrog <phrog@onet.pl>
2021-12-22 08:17:10 +0100

    Added Storage class template

---
+++ storage.py
@@ -0,0 +1,19 @@
+import pathlib
+
+
+class Storage:
+    def __init__(self, file_name, file_path='.'):
+        self.file_name = file_name
+
+    def _if_file_exists(self):
+        pass
+
+    def _create_file(self):
+        pass
+
+    def add_board_game(self, game_name, link, price):
+        pass
+
+
+if __name__ == '__main__':
+    pass
